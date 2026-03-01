7574553d513a0aa364a1af9ce8c1ac2a04252c70
hululu1068 <xjianglin@gmail.com>
2026-03-01 15:05:48 +0800

    Update diy-part01.sh to retain core libraries
    Removed the removal of core libraries from OpenWrt feeds.

--- ArmV8_Virtual/diy-part01.sh
+++ ArmV8_Virtual/diy-part01.sh
@@ -44,6 +44,3 @@ git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git package/
 git clone --depth=1 https://github.com/fw876/helloworld.git package/custom-package/helloworld
 # Others
 git clone --depth=1 https://github.com/chenmozhijin/luci-app-socat.git package/custom-package/luci-app-socat
-
-# 移除 openwrt feeds 自带的核心库
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
